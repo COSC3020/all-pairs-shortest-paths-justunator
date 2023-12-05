b42020f4a1b0685b50f52b091972eb0878883d00
Justunator <quanjustin@ymail.com>
2023-12-05 15:16:59 -0700

    changed the tests

--- code.test.js
+++ code.test.js
@@ -2,33 +2,10 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
-function floydWarshall(graph) { 
-    let dist = [];
-    for (let i = 0; i < graph.length; i++) {
-        dist[i] = [];
-        for (let j = 0; j < graph.length; j++) {
-        if (i === j) {
-            dist[i][j] = 0;
-        } else if (!isFinite(graph[i][j])) {
-            dist[i][j] = Infinity;
-        } else {
-            dist[i][j] = graph[i][j];
-            }
-        }
-    }
-    
-    for (let k = 0; k < graph.length; k++) {
-        for (let i = 0; i < graph.length; i++) {
-            for (let j = 0; j < graph.length; j++) {
-                if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-    
-    return dist;
-} // code taken from: https://reintech.io/blog/javascript-implementing-floyd-warshall-algorithm
-
-g = [[0,2,3,4,1], [1,0,1,4,5], [1,2,0,1,1], [1,2,3,0,5], [1,2,3,4,0]]
-assert(JSON.stringify(allPairsShortestPaths(g)) === JSON.stringify(floydWarshall(g)));
+assert(JSON.stringify(allPairsShortestPaths([[0,2,3,4,1], [1,0,1,4,5], [1,2,0,1,1], [1,2,3,0,5], [1,2,3,4,0]])) === JSON.stringify([[ 0, 2, 3, 4, 1 ],[ 1, 0, 1, 2, 2 ],[ 1, 2, 0, 1, 1 ],[ 1, 2, 3, 0, 2 ],[ 1, 2, 3, 4, 0 ]]));
+assert(JSON.stringify(allPairsShortestPaths([[11,22,3,42,1], [11,0,1,4,5], [1,2,10,1,12], [1,2,3,0,5], [1,2,3,5,7]])) === JSON.stringify([[ 0, 3, 3, 4, 1 ],[ 2, 0, 1, 2, 3 ],[ 1, 2, 0, 1, 2 ],[ 1, 2, 3, 0, 2 ],[ 1, 2, 3, 4, 0 ]]));
+assert(JSON.stringify(allPairsShortestPaths([[11,22], [4,5]])) === JSON.stringify([[ 0, 22 ], [ 4, 0 ]]));
+assert(JSON.stringify(allPairsShortestPaths([])) === JSON.stringify([]));
+assert(JSON.stringify(allPairsShortestPaths([[]])) === JSON.stringify([[0]]));
+assert(JSON.stringify(allPairsShortestPaths([[], [1]])) === JSON.stringify([[0, Infinity], [1, 0]]));
+assert(JSON.stringify(allPairsShortestPaths([[0, Infinity, Infinity], [1,0,1], [4,0,0]])) === JSON.stringify([ [ 0, Infinity, Infinity ], [ 1, 0, 1 ], [ 1, 0, 0 ] ]));
